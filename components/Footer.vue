a9415a9c60517c7c81715551ee86a64140c66d84
m0uka <lukasekmoucka0@gmail.com>
2021-03-27 14:59:46 +0100

    feat: add nuxtjs mention

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="bg-gray-900 p-8 px-20">
         <div class="mx-auto">
             <p class="font-sans text-gray-300 font-bold tracking-wide">m0uka.dev</p>
-            <p class="text-gray-500 font-semibold tracking-wide">Made with Tailwind CSS</p>
+            <p class="text-gray-500 font-semibold tracking-wide">Made with NuxtJS and Tailwind CSS</p>
         </div>
     </div>
 </template>
