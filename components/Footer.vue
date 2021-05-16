5dffd68403880e09608b4f39bd4bcacf21ae4aba
m0uka <lukasekmoucka0@gmail.com>
2021-05-16 14:17:12 +0200

    feat: add footer link to source code

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,6 +3,7 @@
         <div class="mx-auto">
             <p class="font-sans text-gray-300 font-bold tracking-wide">m0uka.dev</p>
             <p class="text-gray-500 font-semibold tracking-wide">Made with NuxtJS and Tailwind CSS</p>
+            <a class="text-gray-400" href="https://github.com/m0uka/m0ukaPortfolio">Source code</a>
         </div>
     </div>
 </template>
