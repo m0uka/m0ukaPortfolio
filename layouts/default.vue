7a0337d2c8d052521106701063bbca887e8e944f
Lukas <31182042+m0uka@users.noreply.github.com>
2021-08-21 12:50:27 +0200

    fix: font fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,6 @@
 
 <style scoped>
   #app {
-    font-family: Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;
+    font-family: 'Inter', sans-serif;
   }
 </style>
