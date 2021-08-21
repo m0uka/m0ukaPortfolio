144d15da35635dded77122d8cdf35cfe40e1df45
Lukas <31182042+m0uka@users.noreply.github.com>
2021-08-21 12:48:02 +0200

    fix: stupid font

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      sans: ['Inter var,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;']
+      sans: ['Inter, sans-serif;']
     },
     extend: {
       lineHeight: {
